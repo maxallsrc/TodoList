72cb57ced604fe213469294b06ddb40780026ff9
maxallsrc <maxallsrc@gmail.com>
2022-05-30 13:20:36 +0600

    TodoService: Swagger added, MS SQL DB, Code-First

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,11 +1,18 @@
-﻿namespace TodoApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
 {
     #region snippet
     public class TodoItem
     {
+        [Key]
         public long Id { get; set; }
+
+        [Required, MaxLength(250)]
         public string Name { get; set; }
+
         public bool IsComplete { get; set; }
+
         public string Secret { get; set; }
     }
     #endregion
